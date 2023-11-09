554ffdd3f525a3a43f720bbaeadc00bf9c8e3481
Vishakha <vishakhachangedia@gmail.com>
2023-11-10 00:03:38 +0530

    Initialize the CRM data generation script.
    Set up the initial structure for the CRM data generation script.

--- Generate_data.py
+++ Generate_data.py
@@ -1,1 +1,16 @@
+from faker import Faker
+import random
+import pandas as pd
+import nltk
+from nltk.corpus import words
 
+fake = Faker('en_IN')  # Use Indian locale for names and addresses
+nltk.download('words')
+
+# Create an empty list to store generated data
+data = []
+
+# Number of records to generate
+num_records = 1000
+
+word_list = words.words()
