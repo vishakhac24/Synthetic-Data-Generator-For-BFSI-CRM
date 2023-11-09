ad91c82500e8bec509cdd668696fbbb61d2fa01a
Vishakha <vishakhachangedia@gmail.com>
2023-11-10 00:19:00 +0530

    Create a function to generate social media handles.
    Define a function to generate relevant social media handles based on customer names.

--- Generate_data.py
+++ Generate_data.py
@@ -22,3 +22,10 @@ negative_keywords = ['Poor', 'Unsatisfactory', 'Disappointed', 'Confusing']
 # Create a function to generate feedback
 def generate_feedback():
     return f"{random.choice(positive_keywords)}: {fake.sentence()}"
+
+# Create a function to generate social media handles
+def generate_social_media_handle(first_name, last_name):
+    first_name_lower = first_name.lower()
+    last_name_lower = last_name.lower()
+    handle = f"{first_name_lower}_{last_name_lower}_{random.randint(10, 99)}"
+    return handle
